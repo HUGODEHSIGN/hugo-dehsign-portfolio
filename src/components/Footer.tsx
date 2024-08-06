e92fd1179656a10b1656b4614b1af35d3bf57c44
Hugo Hsi <hugohsi@pop-os.localdomain>
2024-08-06 14:23:03 -0400

    updated footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,44 +1,40 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import { toast } from 'sonner';
+import { Button } from "@/components/ui/button";
+import { Home } from "lucide-react";
+import Link from "next/link";
+import { toast } from "sonner";
 
 export default function Footer() {
-  return (
-    <footer className="container my-4">
-      <nav className="flex flex-col sm:flex-row gap-6 sm:gap-0 justify-between p-6 bg-primary rounded-lg">
-        <Button variant="outline">Hugo Dehsign</Button>
-        <div className="flex flex-col sm:flex-row items-start gap-2">
-          <Button
-            variant="link"
-            className="text-primary-foreground">
-            <Link
-              href="https://www.linkedin.com/in/hugo-hsi/"
-              target="_blank">
-              LinkedIn
-            </Link>
-          </Button>
-          <Button
-            variant="link"
-            className="text-primary-foreground">
-            <Link
-              href="https://github.com/HUGODEHSIGN/"
-              target="_blank">
-              Github
-            </Link>
-          </Button>
-          <Button
-            variant="link"
-            className="text-primary-foreground"
-            onClick={() => {
-              navigator.clipboard.writeText('hhsi@hugodehsign');
-              toast('Copied to clipboard');
-            }}>
-            hhsi@hugodehsign
-          </Button>
-        </div>
-      </nav>
-    </footer>
-  );
+	return (
+		<footer className="container my-4">
+			<nav className="flex flex-col sm:flex-row gap-6 sm:gap-0 justify-between p-6 bg-primary rounded-lg">
+				<Button variant="outline" size="icon">
+					<Home width={20} height={20} />
+				</Button>
+				<div className="flex flex-col sm:flex-row items-start gap-2">
+					<Button variant="link" className="text-primary-foreground">
+						<Link href="https://www.linkedin.com/in/hugo-hsi/" target="_blank">
+							LinkedIn
+						</Link>
+					</Button>
+					<Button variant="link" className="text-primary-foreground">
+						<Link href="https://github.com/hugo-hsi-dev" target="_blank">
+							Github
+						</Link>
+					</Button>
+					<Button
+						variant="link"
+						className="text-primary-foreground"
+						onClick={() => {
+							navigator.clipboard.writeText("me@hugohsi.dev");
+							toast("Copied to clipboard");
+						}}
+					>
+						me@hugohsi.dev
+					</Button>
+				</div>
+			</nav>
+		</footer>
+	);
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,17 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 type NavbarProps = {
-  children: React.ReactNode;
+	children: React.ReactNode;
 };
 export default function Navbar({ children }: NavbarProps) {
-  return (
-    <header className="flex flex-col sm:flex-row gap-4 justify-between items-center rounded-lg bg-primary shadow mt-6 p-6">
-      <Link href="/">
-        <h1 className="text-3xl font-bold  text-primary-foreground">
-          Hugo Dehsign
-        </h1>
-      </Link>
-      <nav className="">{children}</nav>
-    </header>
-  );
+	return (
+		<header className="flex flex-col sm:flex-row gap-4 justify-between items-center rounded-lg bg-primary shadow mt-6 p-6">
+			<Link href="/">
+				<h1 className="text-3xl font-bold  text-primary-foreground">
+					HUGO HSI
+				</h1>
+			</Link>
+			<nav className="">{children}</nav>
+		</header>
+	);
 }
